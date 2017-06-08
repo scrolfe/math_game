18117ce4d6546a57316afa11b840ae3b25627faa
Sam Rolfe <scrolfe@gmail.com>
2017-06-08 16:42:26 -0500

    added divs and css selectors for each token-seat placeholder

--- JS/script.js
+++ JS/script.js
@@ -15,12 +15,15 @@ var game = {
     },
     //create prompt function that changes instructions/narrative based on game progress
     currentMessage: function () {
+        //loop through progresses and add index to below function to clean up code
         for (var i = 1; i < $('h1').length; i++) {
+            //have different 'progress' provide different messages to the user
             if (game.progress === i) {
                 $('#msg000' + (i - 1)).hide()
                 $('#msg000' + i).show()
         }}
     },
+    //same as above but for directions
     currentDirections: function () {
         for (var i = 1; i < $('p').length; i++) {
             if (game.progress === i) {
