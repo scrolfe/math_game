ec4245fdae5a8ecbde363dd8bf8fba4855a7ac4e
Sam Rolfe <scrolfe@gmail.com>
2017-06-08 16:01:38 -0500

    added for loop to increment message and directions with progress

--- JS/script.js
+++ JS/script.js
@@ -15,14 +15,18 @@ var game = {
     },
     //create prompt function that changes instructions/narrative based on game progress
     currentMessage: function () {
-        if (game.progress === 1) {
-            $('#msg0000').hide()
-        }
+        for (var i = 1; i < $('h1').length; i++) {
+            if (game.progress === i) {
+                $('#msg000' + (i - 1)).hide()
+                $('#msg000' + i).show()
+        }}
     },
     currentDirections: function () {
-        if (game.progress === 1) {
-            $('#dir0000').hide()
-        }
+        for (var i = 1; i < $('p').length; i++) {
+            if (game.progress === i) {
+                $('#dir000' + (i - 1)).hide()
+                $('#dir000' + i).show()
+        }}
     },
 };
 game.currentMessage();
