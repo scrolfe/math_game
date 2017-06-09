ad1091162e62e2dcf487f1f8cee9f47e7403b001
Sam Rolfe <scrolfe@gmail.com>
2017-06-09 14:53:44 -0500

    Added droppability activation and deactivation thru jquery ui

--- JS/script.js
+++ JS/script.js
@@ -32,10 +32,56 @@ var game = {
         }}
     },
 };
+//call message and directions
 game.currentMessage();
 game.currentDirections();
 
-var draggableTokens = function () {
-    $('.token').draggable();
+//make seats react to tokens being dragged
+var activateTokenSeat = function (event, ui) {
+    $('.token-seat').on('dropactivate', function(event, ui) {
+        $('.token-seat').css('height', '6em')
+    })
+};
+
+//stop drag reaction when not being dragged anymore
+var deactivateTokenSeat = function (event, ui) {
+    $('.token-seat').on('dropdeactivate', function(event, ui) {
+        console.log('bunny')
+        $('.token-seat').css('height', '5em')
+    })
 }
+
+//make function for tokens being draggable using jquery-ui
+var draggableTokens = function () {
+    $('.token').draggable({
+        //make cursor change icons on click+drag
+        cursor: 'move',
+        //make cursor prefer edges of .token-seat div
+        snap: '.token-seat'
+    });
+};
+//call draggability of tokens
 draggableTokens();
+//make token-seats droppable
+var droppableTokenSeats = function () {
+    $('.token-seat').droppable({
+        accept: '.token',
+        activate: activateTokenSeat(),
+        deactivate: deactivateTokenSeat(),
+        // drop: tokenSeatReactToDrop(),
+    })
+};
+//call droppability of token-seats
+droppableTokenSeats();
+
+
+//make individual seats change when terms are dropped on them
+// var tokenSeatReactToDrop = function (event, ui) {
+//     //for loop which causes below occurrence on individual token-seat
+//     for (var i = 0; i < $('token-seat').length; i++) {
+//         $('.token-seat').eq(i).on('drop', function(event, ui){
+//             $('.token-seat').css('border-radius', '300px')
+//             }
+//         )
+//     }
+// }
