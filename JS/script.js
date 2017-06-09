152e40abfb693452307a4a2482a4aad937527e37
Sam Rolfe <scrolfe@gmail.com>
2017-06-09 17:54:59 -0500

    further attempts to make divs drop onto other divs and change

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,20 @@
-//make game object
-
+//Game Object
+//Attributes that will be needed for the game/that will keep coming back
 var game = {
-    //determine some attributes that will be needed for the game/keep coming back
+    //Progress is a switch controller for the relationship between game-progress and direction on-screen
     progress: 0,
-    /*make method that changes the messagebox and directionsbox when the user
-    moves to the next section */
+    //Use this to throw rewarding stimuli and prompts to provide teacher email-address to send your score
     won: false,
+    //Possibly used to track successful attempts versus unsuccessful
     score: 0,
+    //For tracking which tables took correctly sorted divs
     tablesComplete: {
         table1: false,
         table2: false,
         table3: false,
         table4: false
     },
-    //create prompt function that changes instructions/narrative based on game progress
+    //Changes the messageBox and directionsBox when the user moves to the next section
     currentMessage: function () {
         //loop through progresses and add index to below function to clean up code
         for (var i = 1; i < $('h1').length; i++) {
@@ -32,71 +33,85 @@ var game = {
         }}
     },
 };
-//call message and directions
+//call message
 game.currentMessage();
+//call directions
 game.currentDirections();
-
+//\/ \/ \/ \/ \/ \/ \/ \/ \/ \/
+//CLICK+DRAG FUNCTIONALITY
+//\/ \/ \/ \/ \/ \/ \/ \/ \/ \/
 //make seats react to tokens being dragged
 var activateTokenSeat = function (event, ui) {
     $('.token-seat').on('dropactivate', function(event, ui) {
+        // //make tokens teeny
+        // $('.token').css('height', '4em')
+        // $('.token').css('width', '6em')
+        //make seats taller
         $('.token-seat').css('height', '6em')
     })
 };
-
 //stop drag reaction when not being dragged anymore
 var deactivateTokenSeat = function (event, ui) {
     $('.token-seat').on('dropdeactivate', function(event, ui) {
         $('.token-seat').css('height', '5em')
     })
 }
-
-//make function for tokens being draggable using jquery-ui
+//change token seats to be circular, change color, and take term when token drops
+// var tokenDropped = function (event, ui) {
+//     $('.token-seat').on('droppabledrop', function (event, ui) {
+//         console.log('testing')
+//         // $('.token').hide()
+//         // $('.token-seat').css('border-radius', '300px')
+//         // $('.token-seat').css('background-color', 'purple')
+//     })
+// }
+//detect when token hovers over a seat and react to this
+// var tokenOverSeat = function (event, ui) {
+//     $('.token-seat').on('dropover', function (event, ui) {
+//         console.log('thing')
+//         $('.token-seat')
+//     })
+// }
+//
+//main jquery ui draggable elements hub
 var draggableTokens = function () {
     $('.token').draggable({
-        //make cursor change icons on click+drag
+        //make cursor change to crosshair icon on drag
         cursor: 'move',
+        //make cursor stay in middle of token
         cursorAt: {left: 75, top: 50},
+        //have token return to position on release
         revert: true,
+        //set dragging to cause window scrolling if need be (didn't have much effect)
         // scroll: true,
         // scrollSensitivity: 25,
         // scrollSpeed: 25,
-        //make cursor prefer edges of .token-seat div
-        snap: '.token-seat'
+        //make cursor prefer edges of .token-seat div (a little clunky)
+        // snap: '.token-seat'
     });
 };
-//call draggability of tokens
+//initiate dragable
 draggableTokens();
-//make token-seats droppable
+//main jquery ui dropable elements hub
 var droppableTokenSeats = function () {
     $('.token-seat').droppable({
+        //should make token-seat accept token
         accept: '.token',
+        //method (see activate...() above)
         activate: activateTokenSeat(),
+        //method (see deactivate...() above)
         deactivate: deactivateTokenSeat(),
-        // drop: tokenSeatReactToDrop(),
+        // drop: tokenDropped(),
+        // over: tokenOverSeat(),
     })
 };
 //call droppability of token-seats
 droppableTokenSeats();
 
-//change token seats to be circular and take term when token drops
-var tokenDropped = function (event, ui) {
-    $('.token-seat').on('droppabledrop', function (event, ui) {
-        console.log('testing')
-        // $('.token').hide()
-        // $('.token-seat').css('border-radius', '300px')
-        // $('.token-seat').css('background-color', 'purple')
-    })
-}
-
-var tokenOverSeat = function (event, ui) {
-    $('.token-seat').on('droppableover', function (event, ui) {
-        console.log('thing')
-    })
-}
-//make function for droppable circularity
-    //for each token-seats
-        //if token drops to seat
+//make function for droppable circularity                                           H
+    //for each token-seats                                                          o
+        //if token drops to seat                                                    w
             //hide token
-            //make token-seat border radius 300px
-            //change token-seat color
-            //have token-seat adopt dropped token's term
+            //make token-seat border radius 300px                                   d
+            //change token-seat color                                               o
+            //have token-seat adopt dropped token's term                            ?
