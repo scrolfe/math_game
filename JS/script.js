05475ca367265a4f886945bc550c97a59a03e086
Sam Rolfe <scrolfe@gmail.com>
2017-08-02 11:34:04 -0500

    reduced number of comments

--- JS/script.js
+++ JS/script.js
@@ -1,86 +1,101 @@
 //Combine Like Terms Math Game
-var game = { //Game Object: Recurring game conditions and attributes
-    slide: 0, //Controls which slide is displayed in the message box
-    tablesCorrect: { table1: 0, table2: 0, table3: 0, table4: 0 }, //Tracks correct tables
-    currentMessage: function () { //Changes the messageBox and directionsBox when the user moves to the next section
-            if (game.slide === 0) { // slide is connected to button
-                $('#msg1').hide() // hide the message before
-                $('#msg0').show() // show message on slide i
-                $('#dir1').hide() // do the same for directions
-                $('#dir0').show()
-            } else if (game.slide === 1) {
-                $('#msg0').hide() // hide the message before
-                $('#msg1').show() // show message on slide i
-                $('#dir0').hide() // do the same for directions
-                $('#dir1').show() }
+var game = {
+  slide: 0, //slides are instructions
+  tablesCorrect: { table1: 0, table2: 0, table3: 0, table4: 0 },
+  //Changes the messageBox and directionsBox when the user moves to the next section
+  currentMessage: function () {
+    if (game.slide === 0) {
+      $('#msg1').hide()
+      $('#msg0').show()
+      $('#dir1').hide()
+      $('#dir0').show()
+    } else if (game.slide === 1) {
+      $('#msg0').hide()
+      $('#msg1').show()
+      $('#dir0').hide()
+      $('#dir1').show()
     }
+  }
 };
-var reviewButton = function () { // changes to instructional slide
-    $('.button2').click(function () {
-        game.slide = 1
-        game.currentMessage() })
+// switch view to instructional slide
+var reviewButton = function () {
+  $('.button2').click(function () {
+    game.slide = 1;
+    game.currentMessage();
+  })
 }
 reviewButton();
-var gameButton = function () { // changes to game directions slide
+// switch view to game directions slide
+var gameButton = function () {
     $('.button3').click(function () {
         game.slide = 0
         game.currentMessage() })
 }
 gameButton();
-var tableCompletion = function () { // Checks is 1 or all tables are correct and complete
-    for (var i = 1; i < $('.family').length + 1; i++) {
-        if (game.tablesCorrect['table' + i] === 4) { // if a table has 4 correct spots filled
-            $('.family' + i).css('border', '3px dashed green') } //dotted green border
-        }
-        if (game.tablesCorrect['table1'] === 4 && game.tablesCorrect['table2'] === 4 && game.tablesCorrect['table3'] === 4 && game.tablesCorrect['table4'] === 4){ //if all of them are green
-            $('.token-seat').css('background-color','gold') } // turn the spots gold
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
-            $('h1').eq(0).text('You win!!')
-            $('p').eq(0).hide()
+// Checks if 1 or all tables are correct and complete
+var tableCompletion = function () {
+  for (var i = 1; i < $('.family').length + 1; i++) {
+    if (game.tablesCorrect['table' + i] === 4) {
+      $('.family' + i).css('border', '3px dashed green') }
+    }
+    if (game.tablesCorrect['table1'] === 4 && game.tablesCorrect['table2'] === 4 && game.tablesCorrect['table3'] === 4 && game.tablesCorrect['table4'] === 4){
+      $('.token-seat').css('background-color','gold')
+      $('html, body').animate({ scrollTop: 0 }, 'fast');
+      $('h1').eq(0).text('You win!!')
+      $('p').eq(0).hide()
+    }
 };
 game.currentMessage();
-var checkAnswer = function (seat) { //See if each token drop arrives at correct token-seats
-        var correctTable = $(seat).data('degree') + 1 //current token-seat data-degree placeholder
-        if ($(seat).data('degree')===$('.ui-draggable-dragging').data('degree')){ //same token/seat degree?
-            $('.family' + correctTable).css('border','1px solid green') //right
-            game.tablesCorrect["table" + correctTable] += 1
-            } else {
-            $('.family' + correctTable).css('border','1px solid red')} //wrong
-            tableCompletion();
+//See if each token drop arrives at correct token-seats
+var checkAnswer = function (seat) {
+  var correctTable = $(seat).data('degree') + 1
+  if ($(seat).data('degree')===$('.ui-draggable-dragging').data('degree')){
+    //right answer
+    $('.family' + correctTable).css('border','1px solid green')
+    game.tablesCorrect["table" + correctTable] += 1
+  } else {
+    //wrong answer
+    $('.family' + correctTable).css('border','1px solid red')}
+    tableCompletion();
 };
-var activateTokenSeat = function (event, ui) { //Seats go to expanded state when user picks up tokens
-    $('.token-seat').on('dropactivate', function(event, ui) {
-        $('.token-seat').css('height', '6em') })
+//Seats go to expanded state when user picks up tokens
+var activateTokenSeat = function (event, ui) {
+  $('.token-seat').on('dropactivate', function(event, ui) {
+    $('.token-seat').css('height', '6em') })
 };
-var deactivateTokenSeat = function (event, ui) { //Seats return to original state when user drops tokens
-    $('.token-seat').on('dropdeactivate', function(event, ui) {
-        $('.token-seat').css('height', '5em')
-    })
+//Seats return to original state when user drops tokens
+var deactivateTokenSeat = function (event, ui) {
+  $('.token-seat').on('dropdeactivate', function(event, ui) {
+    $('.token-seat').css('height', '5em')
+  })
 };
 var resetGame = function () {
-    $('.button1').click(function () {
-        window.location.reload()
-    })
+  $('.button1').click(function () {
+    window.location.reload()
+  })
 }
 resetGame();
-var draggableTokens = function () { //token draggable attributes
-    $('.token').draggable({
-        cursor: 'move', //make cursor change to crosshair icon on drag
-        cursorAt: {left: 37, top: 25}, //make cursor stay in middle of token
-        revert: true, //have token return to position on release
-        scope: '.token', //this and below has to do with z-index
-        stack: '.token' });
+// ui controls
+var draggableTokens = function () {
+  $('.token').draggable({
+    cursor: 'move',
+    cursorAt: {left: 37, top: 25},
+    revert: true,
+    scope: '.token',
+    stack: '.token'
+  })
 };
 draggableTokens();
-var droppableTokenSeats = function () { //token-seat droppable attributes
-    $('.token-seat').droppable({
-        accept: '.token',
-        activate: activateTokenSeat(), //this and below make seats change on pick-up and put-down
-        deactivate: deactivateTokenSeat(),
-        drop: function(event, ui) { //occurence when dropped on target
-            $(this).append($('.ui-draggable-dragging')); //appends dragged token to token seat
-            $('.ui-draggable-dragging').css('position','static'); //resets token formatting for new token positioning
-            checkAnswer(this)}, //checks for data- attr match between token and seat
-        scope: '.token', })
+var droppableTokenSeats = function () {
+  $('.token-seat').droppable({
+    accept: '.token',
+    activate: activateTokenSeat(),
+    deactivate: deactivateTokenSeat(),
+    drop: function(event, ui) {
+      $(this).append($('.ui-draggable-dragging'));
+      $('.ui-draggable-dragging').css('position','static');
+      checkAnswer(this)},
+    scope: '.token',
+  })
 };
 droppableTokenSeats();
